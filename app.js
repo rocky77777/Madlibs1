f15921cf9fe90c280c9410517c242b91a1e222e8
Bill Horst <billhorst7@gmail.com>
2016-03-06 11:03:34 -0700

    add hide and show functionality

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 angular.module("myApp", [])
-	.controller("myCtrl", function($scope) {
-		
-	});
+.controller("myCtrl", function($scope) {
+	$scope.showMadLibs = false;
+	$scope.showInputBoxes = true;
+	$scope.hideInputShowOutput = function() {
+		$scope.showMadLibs = true;
+		$scope.showInputBoxes = false;
+	}
+	$scope.startNewGame = function() {
+		$scope.showMadLibs = false;
+		$scope.showInputBoxes = true;
+		$scope.name = null;
+		$scope.dirtyTask = null;
+		$scope.obnoxiousCelebrity = null;
+		$scope.jobTitle = null;
+		$scope.celebrity = null;
+		$scope.hugeNumber = null;
+		$scope.tediousTask = null;
+		$scope.uselessSkill = null;
+		$scope.adjective = null;
+		$scope.data.gender = null;
+	}
+});
